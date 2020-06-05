eca1ae9ad28095adb51591eae6283fdf8b7a7ea9
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-06-04 21:29:05 -0400

    Rename a (probably) misnamed sample
    This updated the bass drum sample so it matches the the pattern
    used by all the other BD samples.

--- 909/files.js
+++ 909/files.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8513abe8210570c80d3e51b6f8b033245fc0604d20534b956b58db8354c8625
+oid sha256:87e6a5b5fb6fd3f235b8cf987ef00afe1f681d36263832edd30c82613f8ddf97
 size 4027
